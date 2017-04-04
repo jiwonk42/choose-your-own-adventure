5f8b8dae82239543d9bd6574f8d3984f4ede3b13
Jiwon Kang and Larry Eby <jiwon+larry@>
2017-04-04 10:15:22 -0700

    add phone-two component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { PhoneOneComponent } from './phone-one/phone-one.component';
+import { PhoneTwoComponent } from './phone-two/phone-two.component';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +12,10 @@ const appRoutes: Routes = [
   {
     path: 'phone-one',
     component: PhoneOneComponent
+  },
+  {
+    path: 'phone-two',
+    component: PhoneTwoComponent
   }
 
 ];
--- src/app/phone-one/phone-one.component.ts
+++ src/app/phone-one/phone-one.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-phone-one',
   templateUrl: './phone-one.component.html',
   styleUrls: ['./phone-one.component.css']
 })
-export class PhoneOneComponent implements OnInit {
+export class PhoneOneComponent {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
-  ngOnInit() {
+  goToPhoneTwo() {
+    this.router.navigate(['phone-two']);
   }
 
 }
